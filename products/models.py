ce281be122d0ea0d500ea35d18938f65bc4eed8c
dev <dev@A13PC7>
2025-12-13 11:31:07 +0200

    qwqewqeqw

---
+++ products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Film(models.Model):
+    GENRE_CHOICES = [
+    ('action', 'Action'),
+    ('fantasy', 'Fantasy')]
+
+    title = models.CharField(max_length=100)
+    image_url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+    mark = models.IntegerField()
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+
+    def __str__(self):
+        return self.title
---
+++ products/urls.py
@@ -0,0 +1,12 @@
+
+
+
+from django.urls import path, include
+
+from products import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('film/<int:id>/', views.oneProductPage)
+]
