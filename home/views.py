f661846859bf2f49d7165f0c1568a58ab4ac2a86
domaksym69@gmail.com <domaksym69@gmail.com>
2026-01-24 11:17:27 +0200

    serchAndAdminPanel

--- home/views.py
+++ home/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
-from favourites.favorites import get_count_of_favorite_films, get_favorites_films
+from favourites.favorites import get_favorites_films
 
 from products.models import Film
 
 # Create your views here.
 def index(request, filter_by_favorites=False):
     films = Film.objects.all()
+
+    filter_text = request.GET.get("filter_text", "")
+
     if filter_by_favorites:
         fav_ids = get_favorites_films(request)
         films = films.filter(id__in=fav_ids)
+
+    if filter_text:
+        films = films.filter(title__icontains=filter_text)
     
     return render(request, 'home/index.html', {
         'films': films, 
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Film
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'genre')
+    search_fields = ('title','genre')
+    list_filter = ('title', 'genre')
+
+admin.site.register(Film, FilmAdmin)
